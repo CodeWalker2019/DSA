5b7e5df1c9a27a3e9a5d881aeb39bb34ce83f356
Serhii Sharyi <serhiisharyi77@gmail.com>
2024-07-09 11:51:53 +0300

    Added Linked List DS

---
+++ DataStructures/LinkedList/implementation.js
@@ -0,0 +1,115 @@
+class Node {
+    /**
+        Implement singly directed linked list Node class
+     */
+}
+
+class LinkedList {
+    /**
+    * Adds an element to the end of the list.
+    *
+    * @param element element to add
+    */
+    add(element) {
+        throw new Error('Not implemented');
+    }
+
+    /**
+     * Adds a new element to the specific position in the list. In case provided index in out of the list bounds
+     * it returns false
+     *
+     * @param index   an index of new element
+     * @param element element to add
+     */
+    add(index, element) {
+        throw new Error('Not implemented');
+    }
+    
+    /**
+    * Changes the value of an list element at specific position. In case provided index in out of the list bounds it
+    * returns false
+    *
+    * @param index   an position of element to change
+    * @param element a new element value
+    */
+    set(index, element) {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Retrieves an elements by its position index. In case provided index in out of the list bounds it
+    * returns undefined
+    *
+    * @param index element index
+    * @return an element value
+    */
+    get(index) {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Returns the first element of the list. Operation is performed in constant time O(1)
+    *
+    * @return the first element of the list
+    */
+    getFirst() {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Returns the last element of the list. Operation is performed in constant time O(1)
+    *
+    * @return the last element of the list
+    */
+    getLast() {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Removes an elements by its position index. In case provided index in out of the list bounds it
+    * returns undefined
+    *
+    * @param index element index
+    * @return deleted element
+    */
+    remove(index) {
+        throw new Error('Not implemented'); 
+    }
+    
+    
+    /**
+    * Checks if a specific element exists in the list
+    *
+    * @return true if element exist, false otherwise
+    */
+    contains(element) {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Checks if a list is empty
+    *
+    * @return true if list is empty, false otherwise
+    */
+    isEmpty() {
+        throw new Error('Not implemented'); 
+    }
+    
+    /**
+    * Returns the number of elements in the list
+    *
+    * @return number of elements
+    */
+    size() {
+        throw new Error('Not implemented');
+    }
+    
+    /**
+    * Removes all list elements
+    */
+    clear() {
+        throw new Error('Not implemented'); 
+    }
+}
+
+module.exports = LinkedList;
---
+++ DataStructures/LinkedList/test.js
@@ -0,0 +1,103 @@
+const assert = require("assert");
+const LinkedList = require("./implementation");
+
+(function testAdd() {
+    let list = new LinkedList();
+    list.add(10);
+    assert.strictEqual(list.get(0), 10);
+    list.add(20);
+    assert.strictEqual(list.get(1), 20);
+    list.add(30);
+    assert.strictEqual(list.get(2), 30);
+    console.log('Test add passed!');
+})();
+
+(function testAddAtIndex() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(1, 15);
+    assert.strictEqual(list.get(1), 15);
+    assert.strictEqual(list.get(2), 20);
+    console.log('Test add at index passed!');
+})();
+
+(function testSet() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    list.set(1, 25);
+    assert.strictEqual(list.get(1), 25);
+    console.log('Test set passed!');
+})();
+
+(function testGet() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    assert.strictEqual(list.get(0), 10);
+    assert.strictEqual(list.get(1), 20);
+    console.log('Test get passed!');
+})();
+
+(function testGetFirst() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    assert.strictEqual(list.getFirst(), 10);
+    console.log('Test get first passed!');
+})();
+
+(function testGetLast() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    assert.strictEqual(list.getLast(), 20);
+    console.log('Test get last passed!');
+})();
+
+(function testRemove() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    list.add(30);
+    assert.strictEqual(list.remove(1), 20);
+    assert.strictEqual(list.get(1), 30);
+    console.log('Test remove passed!');
+})();
+
+(function testContains() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    assert.strictEqual(list.contains(20), true);
+    assert.strictEqual(list.contains(30), false);
+    console.log('Test contains passed!');
+})();
+
+(function testIsEmpty() {
+    let list = new LinkedList();
+    assert.strictEqual(list.isEmpty(), true);
+    list.add(10);
+    assert.strictEqual(list.isEmpty(), false);
+    console.log('Test is empty passed!');
+})();
+
+(function testSize() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    assert.strictEqual(list.size(), 2);
+    console.log('Test size passed!');
+})();
+
+(function testClear() {
+    let list = new LinkedList();
+    list.add(10);
+    list.add(20);
+    list.clear();
+    assert.strictEqual(list.isEmpty(), true);
+    console.log('Test clear passed!');
+})();
+
+console.log('All tests passed!');
